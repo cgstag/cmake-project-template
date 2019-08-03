001c05fdae5410f06e3b28b4596d322276d73421
Flavio Deroo <flavio.deroo@gmail.com>
2019-08-03 16:33:57 -0300

    Changed header

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <division.h>
 
-static const char *const HEADER = "\nDivider © 2018 Monkey Claps Inc.\n\n";
+static const char *const HEADER = "\nInitializaing Divider...\n\n";
 static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes the result of a fractional division,\n\tand reports both the result and the remainder.\n";
 
 int main(int argc, const char *argv[]) {
